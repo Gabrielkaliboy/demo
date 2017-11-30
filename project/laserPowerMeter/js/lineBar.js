587ece274fb85320db418c9b9b247afd28e5730b
Gabriel_zhu <Gabriel_wei@qq.com>
2017-11-30 09:47:08 +0800

    新增加lineBar

---
+++ project/laserPowerMeter/js/lineBar.js
@@ -0,0 +1,66 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: '堆叠柱形图'
+        },
+        xAxis: {
+            categories: ['苹果', '橘子', '梨', '葡萄', '香蕉']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '水果消费总量'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    '总量: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: '小张',
+            data: [5, 3, 4, 7, 2]
+        }, {
+            name: '小彭',
+            data: [2, 2, 3, 2, 1]
+        }, {
+            name: '小潘',
+            data: [3, 4, 4, 2, 5]
+        }]
+    });
+});
